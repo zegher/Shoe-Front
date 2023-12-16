d242c031e8727595f26bfebc04b565627a4a3c09
zghr <r0878962@student.thomasmore.be>
2023-12-16 16:57:52 +0100

    Feat: use api data

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -4,11 +4,22 @@
             SHOES
         </h1>
 
-        <div>
-            <div border>
-
+        
+        <div class="flex justify-center m-[5%] gap-6">
+            <div>
+                <div class="c-green border-2 border-white h-[200px] w-[150px]"></div>
+                <p class="text-white">{{ shoes.data[0].shoeOrders[0].brand }}</p>
+                <p class="text-white">{{ shoes.data[0].shoeOrders[0].price }}</p>
+                <p class="text-black border-2 border-primary bg-primary mt-[30px]">Price</p>
             </div>
-        </div>
+           
+            <div>
+                <div class="c-green border-2 border-white h-[200px] w-[150px]"></div>
+                <p class="text-white">{{ shoes.data[0].shoeOrders[1].brand }}</p>
+                <p class="text-white">{{ shoes.data[0].shoeOrders[1].price }}</p>
+                <p class="text-black border-2 border-primary bg-primary mt-[30px]">Price</p>
+            </div>                
+        </div>        
     </div>
 
 
