1bc198007e882637aac2d21fea7a7d70c49c671f
zghr <r0878962@student.thomasmore.be>
2023-12-16 15:31:42 +0100

    Add: tailwind.css

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul>
+        <li id="home">
+            Home
+        </li>
+        <li id="order">
+            Order
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+    ul {
+        display: flex;
+        justify-content: space-around;
+        list-style: none;
+        background-color: #64F243;
+        padding: 0;
+    }
+
+    li {
+        padding: 10px;
+        cursor: pointer;
+    }
+
+    li:hover {
+        background-color: #198900;
+        color: white;
+    }
+
+    li.active {
+        background-color: #ccc;
+    }
+</style>
